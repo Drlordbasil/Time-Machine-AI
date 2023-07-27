9e68283e7aa1b0b051ce8d49883c7d91ed11502f
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-27 10:47:36 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,181 @@
+import os
+import shutil
+import datetime
+import subprocess
+import random
+import string
+import json
+
+class TimeMachineAI:
+    def __init__(self, github_email, github_token):
+        self.github_email = github_email
+        self.github_token = github_token
+        self.save_dir = "saved_models"
+        self.model_file = os.path.join(self.save_dir, "time_machine_model.json")
+
+    def generate_idea(self):
+        # Generate a random idea based on a prompt
+        prompts = [
+            "Design a self-driving car",
+            "Create an AI-powered virtual assistant",
+            "Develop a recommendation system for movies",
+            "Build a weather forecasting app",
+            "Design an automated stock trading system",
+            "Create a language translation tool"
+        ]
+        prompt = random.choice(prompts)
+        return f"Idea: {prompt}"
+
+    def generate_prompt(self, idea):
+        # Convert the idea into a detailed prompt
+        prompt = f"""
+# {idea}
+
+# TODO: Add detailed description of the prompt
+# TODO: Define the problem clearly
+# TODO: Plan the solution
+# TODO: Implement the solution
+# TODO: Verify the solution
+# TODO: Explain the solution
+
+"""
+        return prompt
+
+    def generate_program(self, prompt):
+        # Generate a Python program based on the prompt
+        program = f"""
+{prompt}
+
+# TODO: Implement the solution based on the prompt
+
+if __name__ == "__main__":
+    # TODO: Test the program
+    pass
+
+"""
+        return program
+
+    def save_model(self, model):
+        # Save the model to a file
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        with open(self.model_file, "w") as f:
+            json.dump(model, f)
+
+    def load_model(self):
+        # Load the model from file
+        if os.path.exists(self.model_file):
+            with open(self.model_file, "r") as f:
+                return json.load(f)
+        return None
+
+    def update_model(self, model, idea, prompt):
+        # Update the model with the new idea and prompt
+        if "ideas" not in model:
+            model["ideas"] = []
+        if "prompts" not in model:
+            model["prompts"] = []
+        model["ideas"].append(idea)
+        model["prompts"].append(prompt)
+
+    def run_tests(self):
+        # Run tests on the Time-Machine-AI and check for any errors
+        try:
+            # Test generating an idea
+            idea = self.generate_idea()
+            assert isinstance(idea, str), "Idea must be a string"
+
+            # Test generating a prompt
+            prompt = self.generate_prompt(idea)
+            assert isinstance(prompt, str), "Prompt must be a string"
+
+            # Test generating a program
+            program = self.generate_program(prompt)
+            assert isinstance(program, str), "Program must be a string"
+
+            # Test saving and loading the model
+            model = {"ideas": [], "prompts": []}
+            self.update_model(model, idea, prompt)
+            self.save_model(model)
+            loaded_model = self.load_model()
+            assert loaded_model == model, "Loaded model does not match"
+
+            print("Tests passed successfully")
+        except AssertionError as e:
+            print(f"AssertionError: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def create_github_repository(self, repo_name, repo_description):
+        # Create a new GitHub repository
+        try:
+            command = [
+                "gh", "repo", "create", repo_name,
+                "--public", "--confirm",
+                f"--title={repo_name}",
+                f"--description={repo_description}",
+                f"--license=mit",
+                f"--template=None",
+                "--enable-wiki",
+                f"--homepage=https://github.com/{self.github_email}",
+                f"--email={self.github_email}",
+                f"--token={self.github_token}"
+            ]
+            subprocess.run(command, check=True)
+            print(f"Created GitHub repository: {repo_name}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error creating GitHub repository: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def upload_to_github(self, repo_name, repo_path):
+        # Upload files and folders to a GitHub repository
+        try:
+            command = [
+                "gh", "repo", "upload", repo_name, repo_path,
+                "--title", f"{repo_name}-upload",
+                "--message", f"Upload {repo_path}",
+                "--token", self.github_token
+            ]
+            subprocess.run(command, check=True)
+            print(f"Uploaded {repo_path} to GitHub repository: {repo_name}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error uploading to GitHub repository: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def create_project(self, idea):
+        # Create a new project based on the idea
+        try:
+            # Generate prompt and program
+            prompt = self.generate_prompt(idea)
+            program = self.generate_program(prompt)
+
+            # Save prompt and program to files
+            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            project_name = f"project_{timestamp}"
+            project_dir = os.path.join("projects", project_name)
+            os.makedirs(project_dir)
+            prompt_file = os.path.join(project_dir, "prompt.py")
+            program_file = os.path.join(project_dir, "program.py")
+            with open(prompt_file, "w") as f:
+                f.write(prompt)
+            with open(program_file, "w") as f:
+                f.write(program)
+
+            # Save project files to GitHub repository
+            repo_name = f"mytime/{project_name}"
+            self.create_github_repository(repo_name, idea)
+            self.upload_to_github(repo_name, project_dir)
+
+            print(f"Created project: {project_name}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    # Main program to run the Time-Machine-AI
+    github_email = "your_email@example.com"
+    github_token = "your_github_token"
+    ai = TimeMachineAI(github_email, github_token)
+    ai.run_tests()
+    ai.create_project("Design a self-driving car")
