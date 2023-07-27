ba8eeeee538a51ad6a61297662cad24383c18d24
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-27 12:09:36 -0400

    Improved main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import random
 import string
 import requests
 
-API_KEY = "YOUR_API_KEY"
-GITHUB_REPO_URL = "https://github.com/your-username/repository"
-
 
 def generate_idea():
     """
@@ -37,75 +34,21 @@ Bonus points for:
     return prompt
 
 
-def fetch_weather_forecast(location):
+def transform_prompt_to_code(prompt, api_key):
     """
-    Fetches and displays the weather forecast for the next 7 days for a specific location.
-    :param location: The location for which to fetch the weather forecast.
+    Transform the prompt into a functioning Python program.
+    :param prompt: The program prompt.
+    :param api_key: The API key for weather data.
+    :return: A string representing the code.
     """
-    url = f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={location}&days=7"
-
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            forecast = data['forecast']['forecastday']
-
-            for day in forecast:
-                date = day['date']
-                condition = day['day']['condition']['text']
-                temp_c = day['day']['avgtemp_c']
-                temp_f = day['day']['avgtemp_f']
-                print(f"Date: {date}")
-                print(f"Condition: {condition}")
-                print(f"Temperature: {temp_c}°C / {temp_f}°F")
-                print()
-        else:
-            print("Failed to fetch weather forecast.")
-    except requests.exceptions.RequestException as e:
-        print(str(e))
-
-
-def save_code_to_file(code, file_name):
-    """
-    Saves the code to a Python script file.
-    :param code: The code.
-    :param file_name: The name of the file.
-    """
-    with open(file_name, 'w') as file:
-        file.write(code)
-
-
-def upload_to_github(file_name):
-    """
-    Uploads the script to a GitHub repository.
-    :param file_name: The name of the file.
-    """
-    destination_path = os.path.join("github_repo", file_name)
-    shutil.copyfile(file_name, destination_path)
-    os.chdir("github_repo")
-    subprocess.run(["git", "add", file_name])
-    subprocess.run(["git", "commit", "-m", "Added script"])
-    subprocess.run(["git", "push", "origin", "master"])
-
-
-def main():
-    """
-    Main function to generate the Python script and upload it to GitHub.
-    """
-    idea = generate_idea()
-    prompt = convert_to_prompt(idea)
-    code = f"""import requests
+    code = """import requests
 
 def fetch_weather_forecast(location):
-    url = "https://api.weatherapi.com/v1/forecast.json"
-    params = {{
-        "key": "{API_KEY}",
-        "q": location,
-        "days": 7
-    }}
+    api_key = "{}"
+    url = f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={location}&days=7"
     
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
             forecast = data['forecast']['forecastday']
@@ -126,11 +69,48 @@ def fetch_weather_forecast(location):
     
 location = input("Enter a location: ")
 fetch_weather_forecast(location)
-"""
+""".format(api_key)
+
+    return code
+
+
+def save_code_to_file(code, file_name):
+    """
+    Saves the code to a Python script file.
+    :param code: The code.
+    :param file_name: The name of the file.
+    """
+    with open(file_name, 'w') as file:
+        file.write(code)
+
+
+def upload_to_github(repository_url, file_name):
+    """
+    Uploads the script to a GitHub repository.
+    :param repository_url: The URL of the GitHub repository.
+    :param file_name: The name of the file.
+    """
+    destination_path = os.path.join("github_repo", file_name)
+    shutil.copyfile(file_name, destination_path)
+    os.chdir("github_repo")
+    subprocess.run(["git", "add", file_name])
+    subprocess.run(["git", "commit", "-m", "Added script"])
+    subprocess.run(["git", "push", "origin", "master"])
+
+
+def main():
+    """
+    Main function to generate the Python script and upload it to GitHub.
+    """
+    api_key = "YOUR_API_KEY"
+    
+    idea = generate_idea()
+    prompt = convert_to_prompt(idea)
+    code = transform_prompt_to_code(prompt, api_key)
     file_name = f"{ ''.join(random.choices(string.ascii_lowercase, k=10)) }.py"
 
     save_code_to_file(code, file_name)
-    upload_to_github(file_name)
+    upload_to_github("https://github.com/your-username/repository", file_name)
 
     print("Python script generated and uploaded successfully.")
 
