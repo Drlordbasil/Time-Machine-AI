e46776ea51ad856fd1d1255a2dd91ef42078a95b
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-27 11:32:47 -0400

    Improved main.py

--- main.py
+++ main.py
@@ -3,73 +3,78 @@ import subprocess
 import shutil
 import random
 import string
-import time
+import requests
+
 
 def generate_idea():
     # Generate a custom idea
     # TODO: Use advanced AI techniques to generate creative ideas
     return "Create a program that can predict the weather accurately for the next 7 days."
 
+
 def convert_to_prompt(idea):
     # Convert the idea into a detailed prompt
-    prompt = """
-    Create a Python program that predicts the weather accurately for the next 7 days.
+    prompt = """Create a Python program that predicts the weather accurately for the next 7 days.
     
-    Your program should:
-    - Use an API to fetch weather data for a specific location.
-    - Parse the JSON response to extract relevant information.
-    - Display the weather forecast for each day.
-    
-    Bonus points for:
-    - Allowing users to input their desired location.
-    - Providing the option to display the forecast in different units (e.g., Celsius or Fahrenheit).
-    """
+Your program should:
+- Use an API to fetch weather data for a specific location.
+- Parse the JSON response to extract relevant information.
+- Display the weather forecast for each day.
+
+Bonus points for:
+- Allowing users to input their desired location.
+- Providing the option to display the forecast in different units (e.g., Celsius or Fahrenheit).
+"""
     return prompt
+
 
 def transform_prompt_to_code(prompt):
     # Transform the prompt into a functioning Python program
-    code = """
-    import requests
+    code = """import requests
 
-    def fetch_weather_forecast(location):
-        api_key = "YOUR_API_KEY"
-        url = f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={location}&days=7"
+def fetch_weather_forecast(location):
+    api_key = "YOUR_API_KEY"
+    url = f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={location}&days=7"
 
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            forecast = data['forecast']['forecastday']
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        forecast = data['forecast']['forecastday']
             
-            for day in forecast:
-                date = day['date']
-                condition = day['day']['condition']['text']
-                temp_c = day['day']['avgtemp_c']
-                temp_f = day['day']['avgtemp_f']
-                print(f"Date: {date}")
-                print(f"Condition: {condition}")
-                print(f"Temperature: {temp_c}°C / {temp_f}°F")
-                print()
-        else:
-            print("Failed to fetch weather forecast.")
+        for day in forecast:
+            date = day['date']
+            condition = day['day']['condition']['text']
+            temp_c = day['day']['avgtemp_c']
+            temp_f = day['day']['avgtemp_f']
+            print(f"Date: {date}")
+            print(f"Condition: {condition}")
+            print(f"Temperature: {temp_c}°C / {temp_f}°F")
+            print()
+    else:
+        print("Failed to fetch weather forecast.")
     
-    location = input("Enter a location: ")
-    fetch_weather_forecast(location)
-    """
+location = input("Enter a location: ")
+fetch_weather_forecast(location)
+"""
 
     return code
+
 
 def save_code_to_file(code, file_name):
     # Save the code to a Python script file
     with open(file_name, 'w') as file:
         file.write(code)
 
+
 def upload_to_github(repository_url, file_name):
     # Upload the script to a GitHub repository
-    shutil.copyfile(file_name, os.path.join("github_repo", file_name))
+    destination_path = os.path.join("github_repo", file_name)
+    shutil.copyfile(file_name, destination_path)
     os.chdir("github_repo")
     subprocess.run(["git", "add", file_name])
     subprocess.run(["git", "commit", "-m", "Added script"])
     subprocess.run(["git", "push", "origin", "master"])
+
 
 def main():
     idea = generate_idea()
@@ -81,6 +86,7 @@ def main():
     upload_to_github("https://github.com/your-username/repository", file_name)
 
     print("Python script generated and uploaded successfully.")
+
 
 if __name__ == '__main__':
     main()
