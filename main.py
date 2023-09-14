f26901d08017c4e457ab108d86671da48091ea75
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-14 18:05:23 -0400

    Improved main.py

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import math
 
+
 class TimeMachineAI:
     def __init__(self):
         self.schematics = {}
         self.formulas = {}
-        
+
     def add_schematic(self, name, schematic):
         self.schematics[name] = schematic
-        
+
     def add_formula(self, name, formula):
         self.formulas[name] = formula
-        
+
     def remove_schematic(self, name):
         self.schematics.pop(name, None)
-        
+
     def remove_formula(self, name):
         self.formulas.pop(name, None)
-        
+
     def calculate(self, formula_name, variables):
-        formula = self.formulas.get(formula_name)
-        if formula is None:
+        if formula_name not in self.formulas:
             raise ValueError("Formula not found.")
+        formula = self.formulas[formula_name]
         return eval(formula, variables)
-        
+
     def analyze_schematic(self, schematic_name):
-        schematic = self.schematics.get(schematic_name)
-        if schematic is None:
+        if schematic_name not in self.schematics:
             raise ValueError("Schematic not found.")
+        schematic = self.schematics[schematic_name]
         return f"Analyzing schematic: {schematic}"
-        
+
     def generate_report(self):
         report = "Time Machine AI Report:\n"
         report += "Schematics:\n"
@@ -38,17 +39,18 @@ class TimeMachineAI:
         for name, formula in self.formulas.items():
             report += f"{name}: {formula}\n"
         return report
-        
+
+
 def main():
     ai = TimeMachineAI()
-    
+
     ai.add_schematic("Time Machine 1", "Schematic 1")
     ai.add_schematic("Time Machine 2", "Schematic 2")
-    
+
     ai.add_formula("Time Travel Formula", "distance / speed")
     ai.add_formula("Time Dilation Formula", "time / (math.sqrt(1 - (velocity**2/c**2)))")
     ai.add_formula("Time Warp Formula", "(math.exp(4*gravity*time)) / (1 + math.exp(4*gravity*time))")
-    
+
     variables = {
         "distance": 100,
         "speed": 10,
@@ -57,21 +59,22 @@ def main():
         "c": 299792458,
         "gravity": 9.8
     }
-    
+
     result = ai.calculate("Time Travel Formula", variables)
     print(f"Time Travel Result: {result}")
-    
+
     result = ai.calculate("Time Dilation Formula", variables)
     print(f"Time Dilation Result: {result}")
-    
+
     result = ai.calculate("Time Warp Formula", variables)
     print(f"Time Warp Result: {result}")
-    
+
     analysis = ai.analyze_schematic("Time Machine 1")
     print(analysis)
-    
+
     report = ai.generate_report()
     print(report)
-    
+
+
 if __name__ == "__main__":
     main()
