3e97e9865d7ba50792e49769d1dc0b3a25a581c7
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-14 18:06:38 -0400

    Improved main.py

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import math
 
+
 class TimeMachineAI:
     def __init__(self):
         self.schematics = {}
         self.formulas = {}
-        
-    def add_schematic(self, name: str, schematic: str) -> None:
+
+    def add_schematic(self, name, schematic):
+        # Add a schematic to the AI
         self.schematics[name] = schematic
-        
-    def add_formula(self, name: str, formula: str) -> None:
+
+    def add_formula(self, name, formula):
+        # Add a formula to the AI
         self.formulas[name] = formula
-        
-    def remove_schematic(self, name: str) -> None:
+
+    def remove_schematic(self, name):
+        # Remove a schematic from the AI
         self.schematics.pop(name, None)
-        
-    def remove_formula(self, name: str) -> None:
+
+    def remove_formula(self, name):
+        # Remove a formula from the AI
         self.formulas.pop(name, None)
-        
-    def calculate(self, formula_name: str, variables: dict) -> float:
-        formula = self.formulas.get(formula_name)
-        if formula is None:
-            raise ValueError("Formula not found.")
-        
-        return eval(formula, variables, {"math": math})
-        
-    def analyze_schematic(self, schematic_name: str) -> str:
-        schematic = self.schematics.get(schematic_name)
-        if schematic is None:
-            raise ValueError("Schematic not found.")
+
+    def calculate(self, formula_name, variables):
+        # Calculate the result of a formula with given variables
+        if formula_name not in self.formulas:
+            raise ValueError("Formula not found")
+        formula = self.formulas[formula_name]
+        return eval(formula, variables)
+
+    def analyze_schematic(self, schematic_name):
+        # Analyze a specific schematic
+        if schematic_name not in self.schematics:
+            raise ValueError("Schematic not found")
+        schematic = self.schematics[schematic_name]
         return f"Analyzing schematic: {schematic}"
-        
-    def generate_report(self) -> str:
+
+    def generate_report(self):
+        # Generate a report of all schematics and formulas
         report = "Time Machine AI Report:\n"
         report += "Schematics:\n"
         for name, schematic in self.schematics.items():
@@ -39,17 +46,17 @@ class TimeMachineAI:
         for name, formula in self.formulas.items():
             report += f"{name}: {formula}\n"
         return report
-        
-def main() -> None:
+
+
+def main():
     ai = TimeMachineAI()
-    
     ai.add_schematic("Time Machine 1", "Schematic 1")
     ai.add_schematic("Time Machine 2", "Schematic 2")
-    
+
     ai.add_formula("Time Travel Formula", "distance / speed")
     ai.add_formula("Time Dilation Formula", "time / (math.sqrt(1 - (velocity**2/c**2)))")
     ai.add_formula("Time Warp Formula", "(math.exp(4*gravity*time)) / (1 + math.exp(4*gravity*time))")
-    
+
     variables = {
         "distance": 100,
         "speed": 10,
@@ -58,21 +65,22 @@ def main() -> None:
         "c": 299792458,
         "gravity": 9.8
     }
-    
+
     result = ai.calculate("Time Travel Formula", variables)
-    print(f"Time Travel Result: {result}")
-    
+    print(f"Calculation Result of Time Travel Formula: {result}\n")
+
     result = ai.calculate("Time Dilation Formula", variables)
-    print(f"Time Dilation Result: {result}")
-    
+    print(f"Calculation Result of Time Dilation Formula: {result}\n")
+
     result = ai.calculate("Time Warp Formula", variables)
-    print(f"Time Warp Result: {result}")
-    
+    print(f"Calculation Result of Time Warp Formula: {result}\n")
+
     analysis = ai.analyze_schematic("Time Machine 1")
-    print(analysis)
-    
+    print(analysis + "\n")
+
     report = ai.generate_report()
     print(report)
-    
+
+
 if __name__ == "__main__":
     main()
